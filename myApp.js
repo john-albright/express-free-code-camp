d08f48c09cdcd0b08f6533e151f3ec15a8dd1234
John Albright <John@johnalbightsMBP.attlocal.net>
2021-08-16 02:38:42 -0500

    Added comments and the app.use() method to mount the express.static() middleware to the path /public

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
+// Send the index.html file to the root path 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
 });
+
+// Mount the middleware to serve the styles sheet in the public folder
+app.use("/public", express.static(__dirname + "/public"));
 
 console.log("Hello World");
 
